c1bfbe35f795e54a8950e105b697d509168d5ead
azriel <azriel91@gmail.com>
2013-02-03 18:45:21 +1300

    Adding threading classes to Common.

---
+++ Common/im.azriel.common.threading/src/main/cpp/im/azriel/common/threading/Runnable.cpp
@@ -0,0 +1,24 @@
+/*
+ * Runnable.cpp
+ *
+ *  Created on: 3/02/2013
+ *      Author: azriel
+ */
+
+#include "im/azriel/common/threading/Runnable.h"
+
+namespace im {
+namespace azriel {
+namespace common {
+namespace threading {
+
+Runnable::Runnable() {
+}
+
+Runnable::~Runnable() {
+}
+
+} /* namespace threading */
+} /* namespace common */
+} /* namespace azriel */
+} /* namespace im */
---
+++ Common/im.azriel.common.threading/src/main/cpp/im/azriel/common/threading/Thread.cpp
@@ -0,0 +1,40 @@
+/*
+ * Thread.cpp
+ *
+ *  Created on: 3/02/2013
+ *      Author: azriel
+ */
+
+#include "im/azriel/common/threading/Thread.h"
+
+namespace im {
+namespace azriel {
+namespace common {
+namespace threading {
+
+Thread::Thread(Runnable* const runnable) :
+		runnable(runnable), thread(nullptr) {
+}
+
+Thread::~Thread() {
+}
+
+void Thread::start() {
+	this->thread = SDL_CreateThread(runThread, this);
+}
+
+int Thread::join() {
+	int exitCode = 0;
+	SDL_WaitThread(thread, &exitCode);
+	return exitCode;
+}
+
+int Thread::runThread(void* const thread) {
+	Thread* t = reinterpret_cast<Thread*>(thread);
+	return t->runnable->run();
+}
+
+} /* namespace threading */
+} /* namespace common */
+} /* namespace azriel */
+} /* namespace im */
